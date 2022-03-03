2d95509c69916e8c4dd01888fb6cb725cdac20ac
Omen <89894310+OmenDoesStuff@users.noreply.github.com>
2022-03-03 14:08:59 -0500

    Create CodeProcesser.java

---
+++ src/main/java/com/jknstudios/firescript/processor/CodeProcesser.java
@@ -0,0 +1,15 @@
+package com.jknstudios.firescript.processor;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.*;
+
+public class CodeProcesser {
+    public static void executeScript(ArrayList<String> scriptContents) {
+        
+    }
+}
