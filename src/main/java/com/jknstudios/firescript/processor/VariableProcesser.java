0b031c48cb313d92d29f58503b6a0928cd3747c6
Omen <89894310+OmenDoesStuff@users.noreply.github.com>
2022-03-04 10:05:17 -0500

    Update VariableProcesser.java

--- src/main/java/com/jknstudios/firescript/processor/VariableProcesser.java
+++ src/main/java/com/jknstudios/firescript/processor/VariableProcesser.java
@@ -11,5 +11,24 @@ import java.util.ArrayList;
 import java.util.*;
 
 public class VariableProcesser {
-  public Map<String, String> variableMap = new HashMap<>();
+  public static Map<String, String> variableMap = new HashMap<>();
+  private static int i;
+  
+  public static void process(ArrayList<String> scriptContents) throws Exception {
+        for (String sx : scriptContents) {
+          if(sx.contains("Variable::new(\"") {
+            if(sx.contains("::val(\"")) {
+              String[] var_infoarray = sx.split("::");
+              // Variable Format:
+              // Variable::new(varname)::val("Hello, World"):
+              List<String> list = new ArrayList<String>(Arrays.asList(var_infoarray));
+              list.remove("Variable");
+          
+              list.set(1, list.get(1).replace("new(\"", "").replace("\")", ""));
+              list.set(2, list.get(2).replace("val(\")", "").replace("\"):", "");
+            } else {
+              sx.replace("Variable::new(\"", "");
+            }
+          }
+        }
 }
