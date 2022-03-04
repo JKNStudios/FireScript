cba22f5cf267468852aba7cc57eb6001859acdc0
Omen <89894310+OmenDoesStuff@users.noreply.github.com>
2022-03-04 09:48:59 -0500

    Create VariableProcesser.java

---
+++ src/main/java/com/jknstudios/firescript/processor/VariableProcesser.java
@@ -0,0 +1,15 @@
+package com.jknstudios.firescript.processor;
+
+import com.jknstudios.firescript.FireScript;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.*;
+
+public class VariableProcesser {
+  public Map<String, String> variableMap = new HashMap<>();
+}
